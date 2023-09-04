d1489e80df35f66bb0929749f4de8338386b4c91
Laura Mathias <116749645+CEPIZIN@users.noreply.github.com>
2023-09-04 10:17:25 -0300

    V1

---
+++ server.js
@@ -0,0 +1,8 @@
+import app from './src/app.js'
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
---
+++ src/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const books = [
+  { id: 1, title: "The Lord of the Rings" },
+  { id: 2, title: "The Hobbit" }
+];
+
+app.get('/', (req, res) => {
+  res.status(200).send('Node Course');
+});
+
+app.get('/books', (req, res) => {
+  res.status(200).json(books);
+});
+
+app.get('/books/:id', (req, res) => {
+  let index = findBook(req.params.id);
+  res.json(books[index]);
+});
+
+app.post('/books', (req, res) => {
+  books.push(req.body);
+  res.status(201).send('Book has been successfully registered');
+});
+
+app.put('/books/:id', (req, res) => {
+  let index = findBook(req.params.id);
+  books[index].title = req.body.title;
+  res.json(books);
+});
+
+app.delete('/books/:id', (req, res) => {
+  let { id } = req.params;
+  let index = findBook(id);
+  books.splice(index, 1);
+  res.send(`Book ${id} successfully removed`);
+});
+
+function findBook(id) {
+  return books.findIndex(book => book.id == id);
+}
+
+export default app;
