f24fd4457ffa3aa76e2da2597c7573141f52d117
CEPIZIN~ <lauramathias03@gmail.com>
2023-10-12 15:06:29 -0300

    Erro Update handleError

--- src/middleware/errorHandling.js
+++ src/middleware/errorHandling.js
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 function errorHandling(err, req, res, next) {
   if (err instanceof mongoose.Error.CastError) {
     res.status(400).send({ message: "One or more provided data is incorrect." });
+  } else if (err instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(err.errors)
+      .map(err => err.message)
+      .join(";");
+    res.status(400).send(`The following errors were found: ${mensagensErro}`);
   } else {
     res.status(500).send({ message: "Internal Server Error." });
   }
